d99fc95f3b1fed1bfee506b0193cf0a5556d3c17
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-05-09 16:43:47 +0200

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<header>
+    <div id="header_content">
+        <div class="header_div">
+            <img src="images/logo_black.png" alt="Logo" id="logo_header_img">
+        </div>
+        <div class="header_div">
+            <h1>PORTFOLIO</h1>
+        </div>
+        <div class="header_div">
+            <ul>
+                <li><a href="https://alban-martinant-de-preneuf.students-laplateforme.io/cv/">CV</a></li>
+                <li><a href="https://github.com/alban-martinant-de-preneuf">Github</a></li>
+            </ul>
+        </div>
+    </div>
+    <hr>
+</header>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+include_once("includes/header.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <div class="presentation_div">
+        <p>Actuellement en formation de dévelloppement Web à l'école LaPlateforme_, je suis à la recherche d'une alternance pour septembre 2023.</p>
+    </div>
+
+    <div class="projects_div">
+        <div class="project_div">
+            <a href="https://alban-martinant-de-preneuf.students-laplateforme.io/super-week/" target="_blank">
+                <h3 class="project_title">SUPER-WEEK</h3>
+                <!-- <img src="images/super-week.png" alt="super-week" class="project_img"> -->
+                <div class="img_div">
+                    <div class="fadedbox">
+                        <div class="description">Le but de ce projet était d'implémenter altourouter et l'architecture MVC en PHP et d'utiliser l'asyncrone en javascript.</div>
+                    </div>
+                    <img src="images/super-week.png" alt="super-week" class="project_img"/>
+                </div>
+            </a>
+            <a href="https://github.com/alban-martinant-de-preneuf/super-week" target="_blank">
+                <p class="project_github">Github</p>
+            </a>
+        </div>
+        <div class="project_div">
+            <a href="https://alban-martinant-de-preneuf.students-laplateforme.io/voyages/" target="_blank">
+                <h3 class="project_title">VOYAGES</h3>
+                <!-- <img src="images/voyages.png" alt="voyages" class="project_img"> -->
+                <div class="img_div">
+                    <div class="fadedbox">
+                        <div class="description">Le but de ce projet était de consolider les compétences en html et css.</div>
+                    </div>
+                    <img src="images/voyages.png" alt="super-week" class="project_img"/>
+                </div>
+            </a>
+            <a href="https://github.com/alban-martinant-de-preneuf/voyages" target="_blank">
+                <p class="project_github">Github</p>
+            </a>
+        </div>
+        <div class="project_div">
+            <a href="https://alban-martinant-de-preneuf.students-laplateforme.io/site-la-plateforme/accueil.html" target="_blank">
+                <h3 class="project_title">SITE LA PLATEFORME_</h3>
+                <!-- <img src="images/site-la-plateforme.png" alt="site-la-plateforme" class="project_img"> -->
+                <div class="img_div">
+                    <div class="fadedbox">
+                        <div class="description">Reproduction du site laplateforme.io.</div>
+                    </div>
+                    <img src="images/site-la-plateforme.png" alt="super-week" class="project_img"/>
+                </div>
+            </a>
+            <a href="https://github.com/alban-martinant-de-preneuf/site-la-plateforme" target="_blank">
+                <p class="project_github">Github</p>
+            </a>
+        </div>
+        <div class="project_div">
+            <a href="https://alban-martinant-de-preneuf.students-laplateforme.io/fansite" target="_blank">
+                <h3 class="project_title">FANSITE</h3>
+                <!-- <img src="images/fansite.png" alt="fansite" class="project_img"> -->
+                <div class="img_div">
+                    <div class="fadedbox">
+                        <div class="description">Premier project html/css.</div>
+                    </div>
+                    <img src="images/fansite.png" alt="super-week" class="project_img"/>
+                </div>
+            </a>
+            <a href="https://github.com/alban-martinant-de-preneuf/fansite" target="_blank">
+                <p class="project_github">Github</p>
+            </a>
+        </div>
+
+
+
+    </div>
+</body>
+
+</html>
