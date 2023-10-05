112ab4e957a8ea8e2cd819f96c499cc51eb3d27a
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-10-05 13:16:16 +0200

    header.php

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,22 @@
-<header>
-    <div id="header_content">
-        <div class="header_div">
-            <img src="images/logo_black.png" alt="Logo" id="logo_header_img">
-        </div>
-        <div class="header_div">
-            <h1>PORTFOLIO</h1>
-        </div>
-        <div class="header_div">
-            <ul>
-                <li><a href="https://alban-martinant-de-preneuf.students-laplateforme.io/cv/">CV</a></li>
-                <li><a href="https://github.com/alban-martinant-de-preneuf">Github</a></li>
-            </ul>
-        </div>
+<header class="h-16 w-full bg-navy">
+  <div class="w-full h-full justify-between flex flex-row">
+    <div class="space-y-2 ml-3 mt-3 cursor-pointer h-8" id="burger">
+      <span class="block w-8 h-1 bg-gray rounded"></span>
+      <span class="block w-8 h-1 bg-gray rounded"></span>
+      <span class="block w-8 h-1 bg-gray rounded"></span>
     </div>
-    <hr>
+    <!-- menu small screen-->
+    <nav class="fixed w-40 -translate-x-40 h-full bg-blue shadow-xl transition-all duration-500 z-50" id="nav">
+      <ul class="relative pt-16 text-gray space-y-4">
+        <i class="fa-solid fa-xmark absolute top-1 right-2 cursor-pointer text-3xl text-gray-600" id="close-menu"></i>
+        <li class="text-center">Accueil</li>
+        <li class="text-center"><a href="#presentation">Profil</a></li>
+        <li class="text-center"><a href="#skills">Compétences</a></li>
+        <li class="text-center"><a href="#certifs">Certificats</a></li>
+        <li class="text-center"><a href="#contact">Contact</a></li>
+        <li class="sm:hidden"><hr></li>
+        <li class="text-center"><a href="https://github.com/alban-martinant-de-preneuf" target="_blank">GitHub</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
