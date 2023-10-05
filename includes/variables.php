4c72632e1285000aeb8683908a6f47c21361c7b2
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-10-05 13:16:39 +0200

    variables.php

---
+++ includes/variables.php
@@ -0,0 +1,64 @@
+<?php
+
+$certificats = [
+    "Administrez un systeme Linux" => "Administrez_un_systeme_Linux_.jpg",
+    "Adoptez les API REST pour vos projets web" => "Adoptez_les_API_REST_pour_vos_projets_web_.jpg",
+    "Apprenez à programmer avec JavaScript" => "Apprenez_a_programmer_avec_JavaScript_.jpg",
+    "Apprenez à programmer en C" => "Apprenez_a_programmer_en_C_.jpg",
+    "Apprenez à programmer en Python" => "Apprenez_a_programmer_en_Python_.jpg",
+    "Apprenez à utiliser la ligne de commande dans un terminal" => "Apprenez_a_utiliser_la_ligne_de_commande_dans_un_terminal_.jpg",
+    "Apprenez les bases du langage Python" => "Apprenez_les_bases_du_langage_Python_.jpg",
+    "Comprendre le Web" => "Comprendre_le_Web_.jpg",
+    "Concevez votre réseau TCP/IP" => "Concevez_votre_reseau_TCP_IP_.jpg",
+    "Concevez votre site web avec PHP et MySQL" => "Concevez_votre_site_web_avec_PHP_et_MySQL_.jpg",
+    "Créez votre site web avec HTML5 et CSS3" => "Creez_votre_site_web_avec_HTML5_et_CSS3_.jpg",
+    "Débutez avec React" => "Debutez_avec_React_.jpg",
+    "Découvrez le fonctionnement des algorithmes" => "Decouvrez_le_fonctionnement_des_algorithmes_.jpg",
+    "Ecrivez du JavaScript pour le web" => "Ecrivez_du_JavaScript_pour_le_web_.jpg",
+    "Gérez du code avec Git et GitHub" => "Gerez_du_code_avec_Git_et_GitHub_.jpg",
+    "Implémentez vos bases de données relationnelles avec SQL" => "Implementez_vos_bases_de_donnees_relationnelles_avec_SQL_.jpg",
+    "Initiez-vous à Python pour l'analyse de données" => "Initiez-vous_a_Python_pour_l_analyse_de_donnees_.jpg",
+    "Objectif IA : initiez-vous à l'intelligence artificielle" => "Objectif_IA_initiez-vous_a_l_intelligence_artificielle_.jpg",
+    "Passez au Full Stack avec Node.js, Express et MongoDB" => "Passez_au_Full_Stack_avec_Node.js__Express_et_MongoDB_.jpg",
+    "Programmez en orienté objet en PHP" => "Programmez_en_oriente_objet_en_PHP_.jpg",
+    "Sécurisez votre réseau grâce aux VPN et Firewall" => "Securisez_votre_reseau_grace_aux_VPN_et_Firewall_.jpg"
+];
+
+$projects = [
+    "CINETECH" => [
+        "description" => "Projet d'introduction aux APIs, réaliser avec le modèle MVC, altorouter et autoloader.",
+        "image" => "images/cinetech.png",
+        "url" => "https://alban-martinant-de-preneuf.students-laplateforme.io/cinetech",
+        "github" => "https://github.com/alban-martinant-de-preneuf/cinetech"
+    ],
+    "SUPER-WEEK" => [
+        "description" => "Le but de ce projet était d'implémenter altourouter et l'architecture MVC en PHP et d'utiliser l'asyncrone en javascript.",
+        "image" => "images/super-week.png",
+        "url" => "https://alban-martinant-de-preneuf.students-laplateforme.io/super-week/",
+        "github" => "https://github.com/alban-martinant-de-preneuf/super-week"
+    ],
+    "BOUTIQUE EN LIGNE" => [
+        "description" => "Créer un site de e-commerce.",
+        "image" => "images/boutique-en-ligne.png",
+        "url" => "https://alban-martinant-de-preneuf.students-laplateforme.io/boutique-en-ligne/view/",
+        "github" => "https://github.com/alban-martinant-de-preneuf/boutique-en-ligne"
+    ],
+    "VOYAGES" => [
+        "description" => "Le but de ce projet était de consolider les compétences en html et css.",
+        "image" => "images/voyages.png",
+        "url" => "https://alban-martinant-de-preneuf.students-laplateforme.io/voyages/",
+        "github" => "https://github.com/alban-martinant-de-preneuf/voyages"
+    ],
+    "SITE LA PLATEFORME" => [
+        "description" => "Le but de ce projet était de consolider les compétences en html et css.",
+        "image" => "images/site-la-plateforme.png",
+        "url" => "https://alban-martinant-de-preneuf.students-laplateforme.io/site-la-plateforme/accueil.html",
+        "github" => "https://github.com/alban-martinant-de-preneuf/site-la-plateforme"
+    ],
+    "FANSITE" => [
+        "description" => "Le but de ce projet était de consolider les compétences en html et css.",
+        "image" => "images/fansite.png",
+        "url" => "https://alban-martinant-de-preneuf.students-laplateforme.io/fansite/",
+        "github" => "https://github.com/alban-martinant-de-preneuf/fansite"
+    ]
+];
